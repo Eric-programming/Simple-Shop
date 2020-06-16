b57270c1ba5ddd8907668ee49e6a7207485f782b
eric-programming <eric09279@outlook.com>
2020-06-16 07:59:35 -0700

    update bug

--- api/Startup.cs
+++ api/Startup.cs
@@ -10,36 +10,31 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace api
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace api {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             _Configuration = configuration;
         }
 
         private readonly IConfiguration _Configuration;
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddControllers();
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddControllers ();
             //Add Auto Mapper
-            services.AddAutoMapper(typeof(AutoMapping));
+            services.AddAutoMapper (typeof (AutoMapping));
             //Add DB
-            services.AddDbContext<StoreContext>(x =>
-               x.UseSqlite(_Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<StoreContext> (x =>
+                x.UseSqlite (_Configuration.GetConnectionString ("DefaultConnection")));
             //Add Extension for dependency injections
-            services.AddApplicationServices();
+            services.AddApplicationServices ();
             //Add Cors
-            services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
-            {
+            services.AddCors (o => o.AddPolicy ("CorsPolicy", builder => {
                 builder
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .AllowCredentials()
-                    .WithOrigins("http://localhost:4200");
+                    .AllowAnyMethod ()
+                    .AllowAnyHeader ()
+                    .AllowCredentials ()
+                    .WithOrigins ("http://localhost:4200/");
             }));
             // //Add User DB
             // services.AddDbContext<AppIdentityDbContext> (x => {
@@ -47,20 +42,19 @@ namespace api
             // });
 
             //Add Identity Services
-            services.AddIdentityServices(_Configuration);
+            services.AddIdentityServices (_Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
             // if (env.IsDevelopment())
             // {
             //     app.UseDeveloperExceptionPage();
             // }
-            app.UseMiddleware<ExceptionMiddleware>();
+            app.UseMiddleware<ExceptionMiddleware> ();
             // app.UseStatusCodePagesWithReExecute("/errors/{0}");
             // app.UseHttpsRedirection();
-            app.UseRouting();
+            app.UseRouting ();
             /**
             app.UseStaticFiles();
             app.UseStaticFiles(new StaticFileOptions
@@ -70,13 +64,12 @@ namespace api
                 ), RequestPath = "/content"
             });
             */
-            app.UseCors("CorsPolicy");
-            app.UseAuthentication();
-            app.UseAuthorization();
+            app.UseCors ("CorsPolicy");
+            app.UseAuthentication ();
+            app.UseAuthorization ();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
+            app.UseEndpoints (endpoints => {
+                endpoints.MapControllers ();
             });
             /**
             
